3378d264cd4424499b0352fdc8c9d0012bbc7c9f
shadow6444 <shadowgarden644@gmail.com>
2024-10-07 17:47:50 +0500

    Update StartPage.jsx

--- src/components/pages/StartPage/StartPage.jsx
+++ src/components/pages/StartPage/StartPage.jsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
 import "./StartPage.css";
 
 const StartPage = () => {
-  return <h1>start page here</h1>;
+  return (
+    <section className="start-page-container">
+      <div className="start-page-text">
+        <h2>
+          Test
+          <br />
+          your React skills
+          <br />
+          with quick, fun questions!
+        </h2>
+        <Link to='quiz' className="start-quiz">Start Quiz</Link>
+      </div>
+      <div className="start-page-image">
+        <img src="/start-page.svg" alt="start page image" />
+      </div>
+    </section>
+  );
 };
 
 export default StartPage;
