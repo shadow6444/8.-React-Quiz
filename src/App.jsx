f9f2ac2834a621642212e1ab22852b728202534d
shadow6444 <shadowgarden644@gmail.com>
2024-10-07 22:59:03 +0500

    Added Quiz Path

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import MainLayout from "./components/Layouts/MainLayout";
 import StartPage from "./components/pages/StartPage";
 import PageNotFound from "./components/pages/PageNotFound";
+import Quiz from "./components/pages/Quiz";
 
 const App = () => {
   return (
@@ -12,7 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<StartPage />} />
-
+          <Route path="quiz" element={<Quiz />} />
           <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
