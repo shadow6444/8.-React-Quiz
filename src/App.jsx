feebe6ae0b1bbe093917d05268eb43361d9ae05b
shadow6444 <shadowgarden644@gmail.com>
2024-10-07 17:21:50 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import "./App.css";
 
-function App() {
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
+import MainLayout from "./components/Layouts/MainLayout";
+import StartPage from "./components/pages/StartPage";
+import PageNotFound from "./components/pages/PageNotFound";
+
+const App = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<StartPage />} />
 
-export default App
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
