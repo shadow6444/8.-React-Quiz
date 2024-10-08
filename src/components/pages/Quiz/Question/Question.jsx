27a1308f16cccf207fdafe4c672919aed9f66133
shadow6444 <shadowgarden644@gmail.com>
2024-10-08 21:40:56 +0500

    Delete Question.jsx

--- src/components/pages/Quiz/Question/Question.jsx
+++
@@ -1,8 +0,0 @@
-import { memo } from "react";
-import "./Question.css";
-
-const Question = ({ currentQuestion }) => {
-  return <h1 className="quiz-question">{currentQuestion}</h1>;
-};
-
-export default memo(Question);
